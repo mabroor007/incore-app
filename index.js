7c2000b37e5aa07ffbbfc24256dffc38dd1dc85b
Mabroor007 <amabroor007@gmail.com>
2020-10-17 13:52:29 -0700

    configured 404 page

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(express.static("Public"));
 // routes
 app.use("/", Router);
 
+app.use((req, res) => {
+  res.send("404 not found");
+});
 // app start
 app.listen(4000, () => {
   console.log("Listening on port 4000...");
